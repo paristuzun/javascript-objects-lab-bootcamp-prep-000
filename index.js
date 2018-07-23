33f93575347dce1804955c28950c8349d04fe35f
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 06:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var recipes = new Object({ key1: 'value1' })
 
 function updateObjectWithKeyAndValue(obj, key, value) {
  
-  return Object.assign({}, obj, { [key]: value })
+  return Object.assign({}, obj, { [key]: "value2" })
 }
 
- 
-/*updateObjectWithKeyAndValue(recipes, 'chocolate', '1 cup')*/
+
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = 'value1';
