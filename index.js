c9ad032a1a3d84a5ac66b658624c4dde028b6fb0
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 06:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  const cloneObj = Object.assign({}, object)
+ 
   delete cloneObj[key]
   return cloneObj
 }
