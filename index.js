65463ce7af46e5accca037f068d438985ebf189b
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 06:16:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 updateObjectWithKeyAndValue(recipes, 'chocolate', '1 cup')
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  object.key = 'value1';
 }
 
 function deleteFromObjectByKey(object, key) {
