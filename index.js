f7af06c91f4c31acb6be0b35d762614547d5d035
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 06:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
   return object
 }
 
