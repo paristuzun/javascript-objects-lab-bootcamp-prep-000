3a6aa48f4d11a662de273e5bc43f8f92cd668304
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 06:20:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = new Object({ key1: 'value1' })
 
 function updateObjectWithKeyAndValue(obj, key, value) {
  
-  return Object.assign({}, obj, { [key]: "value2" })
+  return Object.assign({}, obj, { [key]: value2 })
 }
 
 
